709338612855cc434f9243dc1ceb45682f17f038
Pedrohvs <pedrohvs@al.insper.edu.br>
2025-09-15 15:30:28 -0300

    deploy-passo1

---
+++ src/test/java/com/example/prova_intermediaria/ProvaIntermediariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prova_intermediaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaIntermediariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
